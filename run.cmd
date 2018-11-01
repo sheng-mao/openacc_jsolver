c2c1bb8adc69020229719c3271b53ee0de8611e9
maosheng <maosheng@seas.upenn.edu>
2018-11-01 15:26:39 -0400

    compiled using new pgi

--- run.cmd
+++ run.cmd
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -c 1
+#SBATCH -c 8
 #SBATCH --time 00:05:00
 #SBATCH --reservation=bootcamp
 
 #set up environment
 
 #module load openmpi/intel-17.0 intel/17.0
-#module load pgi/17.3/64
+module load pgi/18.5/64
 
 #launch job with srun, not mpirun/mpiexec
-./jsolvec.exe
-#make c++ OPT="-tp haswell-64 -ta=multicore -Minfo=accel"
+#./jsolvec.exe
+make c++ OPT="-tp haswell-64 -ta=multicore -Minfo=accel"
